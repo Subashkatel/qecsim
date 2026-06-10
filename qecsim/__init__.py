e89e2cb1db0afff5a084504025e1094e2178bb46
subashkatel <skatel@ucsd.edu>
2026-06-09 20:06:04 -0400

    exports: SwitchingDecoder, ParallelWindowScheme, deadline policies, new metrics

--- qecsim/__init__.py
+++ qecsim/__init__.py
@@ -7,16 +7,22 @@ from .wiring import build_and_run
 from .frontends.circuit import (CircuitFrontend, SurgeryIRFrontend,
                                 three_cnot_circuit, cnot_plus_two_t_circuit,
                                 independent_t_circuit)
-from .decoders import PresetLatencyDecoder, RelayBPDecoder
+from .decoders import PresetLatencyDecoder, RelayBPDecoder, SwitchingDecoder
 from .factories import InfiniteFactory, DistillationFactory
-from .metrics import DecoderUtilization, ReadyQueueStats
+from .schemes import ParallelWindowScheme
+from .schedulers import EarliestDeadlineScheduler, ReactionPathDeadline
+from .metrics import (DecoderUtilization, ReadyQueueStats,
+                      WindowLatencyBreakdown, MagicStateLatency)
 
 __all__ = [
     "SimConfig", "us", "fmt",
     "build_and_run",
     "CircuitFrontend", "SurgeryIRFrontend",
     "three_cnot_circuit", "cnot_plus_two_t_circuit", "independent_t_circuit",
-    "PresetLatencyDecoder", "RelayBPDecoder",
+    "PresetLatencyDecoder", "RelayBPDecoder", "SwitchingDecoder",
     "InfiniteFactory", "DistillationFactory",
+    "ParallelWindowScheme",
+    "EarliestDeadlineScheduler", "ReactionPathDeadline",
     "DecoderUtilization", "ReadyQueueStats",
+    "WindowLatencyBreakdown", "MagicStateLatency",
 ]
